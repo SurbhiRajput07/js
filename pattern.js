6d3e18137f8be870d345a3d1a72ee6381d50adfe
@SurbhiRajput <surbhiksingh01@gmail.com>
2026-06-16 00:53:43 +0530

    practicing pattern  question: solve basic star pattern question

---
+++ pattern.js
@@ -0,0 +1,48 @@
+
+
+
+//for(i=0;i<5;i++){
+    //   let row="";
+    //   for(j=0;j<5;j++){
+    //     row+="*";
+        
+    //   }
+    //   console.log(row);
+    // }
+    
+    // for(i=0;i<5;i++){
+    //     let row="";
+    //   for(j=0;j<i;j++){
+    //      row+="*";
+          
+    //      }
+    //      console.log(row);
+    // }
+    
+    
+    // for(i=1;i<=5;i++){
+    //   let num="";
+    // for(j=1;j<=i;j++){
+    //     num+=j;
+     
+    //    }
+    //    console.log(num);
+    // }
+    
+    // for(i=1;i<=5;i++){
+    //   let num1="";
+    // for(j=1;j<=i;j++){
+    //   num1+=i;
+     
+    //    }
+    //    console.log(num1);
+    // }
+    
+    for(i=1;i<=5;i++){
+           let row="";
+      for(j=1;j=5-i+1;j++){
+          row+=j;
+            
+            }
+         console.log(row);
+     }
